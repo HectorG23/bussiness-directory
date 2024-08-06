905be1f39e62553f8572a1521b3bea591b705f48
HectorG23 <hdgc2006@gmail.com>
2024-08-05 22:03:26 -0500

    se creo el header y una parte del home

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,24 @@ import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { Text } from "react-native";
 import LoginScreen from './../components/LoginScreen'
+import * as SecureStore from "expo-secure-store"
+
+const tokenCache = {
+  async getToken(key){
+    try{
+      return SecureStore.getItemAsync(key);
+    }catch(err){
+      return null;
+    }
+  },
+  async saveToken(key, value){
+    try{
+      return SecureStore.setItemAsync(key,value)
+    }catch(err){
+      return
+    }
+  }
+}
 export default function RootLayout() {
   useFonts({
     'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
@@ -10,7 +28,7 @@ export default function RootLayout() {
     'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
   })
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SignedIn>
         <Stack screenOptions={{
           headerShown:false
